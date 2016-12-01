5ca84824dab571f1ab3b9e3aed9f42daea8bf390
nelson1987 <nelson.ash@outlook.com>
2016-12-01 09:43:56 -0200

    Projeto com Teste
    Inclusão de arquivos pela necessidade de incluí-los na mão

---
+++ 1_Domain/Tests/Entities/FistTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mimir.Domain.Test.Entities
+{
+    [TestClass]
+    public class FirstTests
+    {
+        [TestMethod]
+        public void Validar_Primeiro_Teste()
+        {
+            EntidadeTeste entidadeTeste = new EntidadeTeste();
+            Assert.AreEqual(entidadeTeste, new EntidadeTeste());
+        }
+    }
+}
